7887d54ee351d263c286270d3664a02031fd0b17
Alistair Cooke <alistair.cooke77@gmail.com>
2018-12-11 20:54:46 +0000

    Added some examples of using [Values] and [Theory] attributes

---
+++ CombinationFixture.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    public class CombinationFixture
+    {
+        public enum ExampleEnum {
+            Yes,
+            No,
+            Maybe
+        }
+
+        [Test]
+        public void CombinationExample([Values] ExampleEnum enumValue, [Values(0,-1,10)] int number) {
+            Assert.Pass();
+        }
+    }
+}
